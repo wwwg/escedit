c6d5bd90411b562844caf60b9dadd7b3f1861998
ultratypebot <xxperipsis@gmail.com>
2017-10-13 20:26:32 -0500

    Add load method of session

--- src/Session.js
+++ src/Session.js
@@ -4,6 +4,11 @@ const SaveData = require('./SaveData'),
 	Session = class Session {
 		constructor() {
 			this.saveDir = os.homedir() + "/The Escapists";
+			console.log('Reading saves directory "' + this.saveDir + '"');
+			this.load().catch(console.log);
 		}
+	}
+	Session.prototype.load = async () => {
+		this.savePaths = await fss.readDir(this.saveDir);
 	}
 module.exports = Session;
