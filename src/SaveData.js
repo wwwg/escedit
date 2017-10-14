768053e727687cfd831bacfa84f8b4d1485ef98d
ultratypebot <xxperipsis@gmail.com>
2017-10-13 21:30:20 -0500

    Add save number to SaveData constructor

--- src/SaveData.js
+++ src/SaveData.js
@@ -2,11 +2,12 @@ const Blowfish = require('./lib/Blowfish'),
 	key = require('./lib/Key'),
 	fss = require('./lib/fss'),
 	SaveData = class SaveData {
-		constructor(rawSave) {
+		constructor(rawSave, saveNum) {
 			if (!rawSave instanceof Buffer) {
 				throw new Error("SaveData must be constructed with a raw buffer.");
 			}
 			this.raw = rawSave;
+			this.num = saveNum;
 			// Decrypt the save into plaintext
 			let bf = new Blowfish(key);
 		}
