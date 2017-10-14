6f1e5b2a37fc69f55fbf33c53bc4e4f1036ca8aa
ultratypebot <xxperipsis@gmail.com>
2017-10-13 23:22:05 -0500

    Add very basic template parser

--- src/SaveContent.js
+++ src/SaveContent.js
@@ -2,6 +2,19 @@
 const SaveContent = class SaveContent {
 	constructor(str) {
 		this.str = str;
+		this.tree = {};
+		this.parse();
+	}
+	parse() {
+		let str = this.str,
+		lns = str.split('\n');
+		for (var i = 0; i < lns.length; ++i) {
+			const ln = lns[i];
+			if (ln.startsWith('[')) {
+				// Category
+				console.log("Category:", ln);
+			}
+		}
 	}
 }
 
