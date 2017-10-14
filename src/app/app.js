c9cbb4f47bb33a925617ee7ab1fba137338c728d
ultratypebot <xxperipsis@gmail.com>
2017-10-14 16:49:53 -0500

    List all parsed saves on UI

--- src/app/app.js
+++ src/app/app.js
@@ -7,4 +7,21 @@ console.log('escedit v0.0.1');
 let s = remote.getGlobal("session");
 s.on('load', () => {
 	console.log('Session has loaded.');
+	$("#save-loading").remove(); // Remove "loading" element
+	for (var i = 0; i < s.saves.length; ++i) {
+		const saveNumber = (i + 1).toString(),
+			nameData = s.saves[i].nameContent.tree["Data"],
+			pname = nameData["Name"],
+			day = nameData["Day"],
+			map = nameData["Map"],
+			saveElem = $(`
+				<div id="save${saveNumber}" class="save">
+					Name: <b id="save${saveNumber}-name"></b><br>
+					Day: <b>${day}</b><br>
+					Map: <b>${map}</b><br>
+				</div>
+			`);
+		$("#save-list").append(saveElem);
+		$(`#save${saveNumber}-name`).text(pname); // To avoid odd XSS issues
+	}
 });
