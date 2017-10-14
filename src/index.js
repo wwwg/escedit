05407903529a8423109501b13ad33fa40d5f6ffc
ultratypebot <xxperipsis@gmail.com>
2017-10-14 00:27:25 -0500

    Implement electron app in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-const Session = require('./Session'),
-	s = new Session();
+const ENABLE_DEV_TOOLS = true,
+	Session = require('./Session'),
+	url = require('url'),
+	path = require('path'),
+	{ app, BrowserWindow } = require('electron');
+let w; // Browser window
+
+app.on('ready', () => {
+	w = new BrowserWindow({
+		center: true,
+		minWidth: 500,
+		minHeight: 500,
+		resizable: true,
+		fullscreenable: true,
+		backgroundColor: '#000'
+	});
+});
