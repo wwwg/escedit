b88341d3ea0b72b2f9669133c3a03ef755a7f0d5
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-10-14 13:35:09 -0500

    retry mcrypt

--- src/saves/SaveData.js
+++ src/saves/SaveData.js
@@ -1,4 +1,6 @@
 const key = require('../lib/Key'),
+	crypto = require('crypto'),
+	MCrypt = require('mcrypt').MCrypt,
 	fss = require('../lib/fss'),
 	SaveContent = require('./SaveContent'),
 	SaveData = class SaveData {
@@ -9,21 +11,17 @@ const key = require('../lib/Key'),
 			this.raw = rawSave;
 			this.num = saveNum;
 			// Compute the amount of padding needed in order for the algorithm to accept the string
-			const padding = (8 - (this.raw.length % 8)) % 8;
+			let raw = rawSave,
+				padding = (8 - (raw.length % 8)) % 8;
 			// Append the padding accordingly in null bytes
 			for (var i = 0; i < padding; ++i) {
-				this.raw += '\0';
+				raw += '\0';
 			}
-			// Decrypt the save into plaintext
-			/*
-			let bf = new MCrypt('blowfish-compat', 'ecb');
-			bf.validateKeySize(false);
-			bf.open(key, bf.generateIv());
-			this.dec = bf.decrypt(this.raw);
-			console.log('Decrypted save ' + this.num + " (length: " + this.dec.length + ")");
-			// Parse save into an AST for easy manipulation
-			this.cont = new SaveContent(this.dec.toString());
-			*/
+			const decipher = new MCrypt('blowfish-compat', 'ecb');
+			decipher.validateKeySize(false);
+			decipher.open(key);
+			let dec = decipher.decrypt(raw);
+			console.log(new Buffer(dec).toString('utf8'));
 		}
 		/*
 		export() {
