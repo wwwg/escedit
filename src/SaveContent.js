6a91669758ff80d9f360fe5f4596172e14af5346
ultratypebot <xxperipsis@gmail.com>
2017-10-13 23:44:07 -0500

    Improve string to number conversions

--- src/SaveContent.js
+++ src/SaveContent.js
@@ -32,16 +32,19 @@ const SaveContent = class SaveContent {
 					// Assignment with multiple = chars, join the rest back together
 					vvalue = parts.slice(1).join('=');
 				}
-				if (!isNaN(vvalue)) {
+				if (!isNaN(vvalue) && !vvalue.includes('_') && vvalue !== '') {
 					// Convert to number as needed
 					vvalue = parseFloat(vvalue);
 				} else if (vvalue.includes('@')) {
 					// Variables are split into arrays with an "@" char as a deliminator
 					vvalue = vvalue.split('@');
 					for (let j = 0; j < vvalue.length; ++j) {
-						if (!isNaN(vvalue[j])) {
+						if (!isNaN(vvalue[j]) && !vvalue.includes('_')) {
 							// Convert to number as needed
-							vvalue[j] = parseFloat(vvalue[j]);
+							const newVal = parseFloat(vvalue[j]);
+							if (!isNaN(newVal)) {
+								vvalue[j] = parseFloat(vvalue[j]);
+							}
 						}
 					}
 				}
