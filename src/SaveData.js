727b8974bb6c2a91f2b74c48fed52414d032e678
ultratypebot <xxperipsis@gmail.com>
2017-10-13 21:35:54 -0500

    Convert save to string

--- src/SaveData.js
+++ src/SaveData.js
@@ -6,10 +6,12 @@ const Blowfish = require('./lib/Blowfish'),
 			if (!rawSave instanceof Buffer) {
 				throw new Error("SaveData must be constructed with a raw buffer.");
 			}
-			this.raw = rawSave;
+			this.raw = rawSave.toString('utf8');
 			this.num = saveNum;
 			// Decrypt the save into plaintext
 			let bf = new Blowfish(key);
+			this.dec = bf.decrypt(this.raw);
+			console.log('Decrypted save #' + this.num);
 		}
 	}
 module.exports = SaveData;
