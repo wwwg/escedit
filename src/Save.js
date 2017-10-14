2ced8c47a8369cf65b8bdd0a3f35fd4d85c40615
ultratypebot <xxperipsis@gmail.com>
2017-10-13 20:53:04 -0500

    Read save dir contents

--- src/Save.js
+++ src/Save.js
@@ -1,13 +1,16 @@
 // Abstract representation of an entire The Escapists save directory.
-const Save = class Save {
-	constructor(path) {
-		if (!path) {
-			throw new Error("Save constructed with invalid path.");
-			return;
+const fss = require('./lib/fss'),
+	Save = class Save {
+		constructor(path) {
+			if (!path) {
+				throw new Error("Save constructed with invalid path.");
+				return;
+			}
+			this.path = path;
+			this.content = null;
 		}
 	}
-}
 Save.prototype.load = async s => {
-	console.log(s);
+	s.content = fss.readDir(s.path);
 }
 module.exports = Save;
