a9ba34e4baf144c6ee07bdb789e38d7f8a7556f3
ultratypebot <xxperipsis@gmail.com>
2017-10-13 19:54:22 -0500

    Import Blowfish.js

--- src/SaveData.js
+++ src/SaveData.js
@@ -1,8 +1,9 @@
-const SaveData = class SaveData {
-	constructor(rawSave) {
-		if (!rawSave instanceof Buffer) {
-			throw new Error("SaveData must be constructed with a raw buffer.");
+const Blowfish = require('./lib/Blowfish'),
+	SaveData = class SaveData {
+		constructor(rawSave) {
+			if (!rawSave instanceof Buffer) {
+				throw new Error("SaveData must be constructed with a raw buffer.");
+			}
 		}
 	}
-}
 module.exports = SaveData;
