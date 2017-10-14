0b69fadcc5328ab979989cd5692dea4923922ea7
ultratypebot <xxperipsis@gmail.com>
2017-10-13 20:39:06 -0500

    Add save constructor and load method

---
+++ src/Save.js
@@ -0,0 +1,13 @@
+// Abstract representation of an entire The Escapists save directory.
+const Save = class Save {
+	constructor(path) {
+		if (!path || !(path instanceof String)) {
+			throw new Error("Save constructed with invalid path.");
+			return;
+		}
+	}
+}
+Save.prototype.load = async () => {
+	console.log(this);
+}
+module.exports = Save;
