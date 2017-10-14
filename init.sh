83db4ae1e93cee5ff318f4eda77ba104a6f79b8e
ultratypebot <xxperipsis@gmail.com>
2017-10-13 19:47:42 -0500

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+node_version=$(node -v)
+if [ "$node_version" != "v8.7.0" ]; then
+	echo -n "You need at least node 8.7.0 in order to run escedit. Your current node version: "
+	echo $node_version
+	exit 0
+fi
+clear
+node --harmony src/index.js
