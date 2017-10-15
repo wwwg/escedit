fc3f740a41932be1d2943444d0eafd095918494f
ultratypebot <xxperipsis@gmail.com>
2017-10-14 21:15:02 -0500

    Create save content from save ASTs

--- src/Export.js
+++ src/Export.js
@@ -1,8 +1,11 @@
+const SaveContent = require('./saves/SaveContent');
 class Export {
 	constructor(nameContent, saveContent, saveNumber) {
 		this.num = saveNumber;
 		this.nameAST = nameContent;
 		this.saveAST = saveContent;
+		this.saveCont = new SaveContent(this.saveAST);
+		this.nameCont = new SaveContent(this.nameAST);
 	}
 }
 
