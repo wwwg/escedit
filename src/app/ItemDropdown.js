be2dac3fbb9b45575c3b88456ac148e5d68363a7
wwwg <xxperipsis@gmail.com>
2017-10-16 13:16:13 -0500

    Add dynamic option creaton

--- src/app/ItemDropdown.js
+++ src/app/ItemDropdown.js
@@ -1,7 +1,16 @@
+const ItemDict = require('../lib/ItemDict');
 class ItemDropdown {
 	constructor(initItem) {
 		this.initial = initItem;
 		this.elm = $(`<select class="item-dropdown>"</select>`);
+		const me = this;
+		ItemDict.forEach(i => {
+			me.elm.append($(`
+				<option name="${i.id}">
+					${i.name}
+				</option>
+			`));
+		})
 	}
 	addTo(parentElem) {
 		$(parentElem).append(this.elm);
