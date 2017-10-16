e8505d4381c682144d9932e38019db1237389368
wwwg <xxperipsis@gmail.com>
2017-10-16 13:55:18 -0500

    Implement <no item> item

--- src/app/ItemDropdown.js
+++ src/app/ItemDropdown.js
@@ -3,6 +3,7 @@ class ItemDropdown {
 	constructor(initItem) {
 		this.initial = initItem;
 		this.elm = $(`<select class="item-dropdown>"</select>`);
+		this.elm.append(`<option value="-1">None</option>`);
 		const me = this;
 		ItemDict.forEach(i => {
 			me.elm.append($(`
@@ -11,10 +12,16 @@ class ItemDropdown {
 				</option>
 			`));
 		});
-		this.elm.val(initItem.toString());
+		if (initItem != '') {
+			this.elm.val(initItem.toString());
+		} else {
+			this.elm.val('-1'); // No item
+		}
+		itemDropdowns.push(this);
 	}
 	addTo(parentElem) {
 		$(parentElem).append(this.elm);
 	}
 }
 window.ItemDropdown = ItemDropdown; // Verify the class is global
+window.itemDropdowns = [];
