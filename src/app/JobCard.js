a4053729adcb1b5b7635fc8bb2bd70c788a2aaa0
wwwg <xxperipsis@gmail.com>
2017-10-17 13:00:22 -0500

    Add JobCard element creation

--- src/app/JobCard.js
+++ src/app/JobCard.js
@@ -2,6 +2,13 @@ class JobCard {
 	constructor(jobName, job) {
 		this.data = job;
 		this.name = jobName;
+		this.elm = $(`<div class="inmate-card">
+			
+		</div>`);
+	}
+	addTo(parent) {
+		const me = this;
+		$(parent).append(this.elm);
 	}
 }
 
