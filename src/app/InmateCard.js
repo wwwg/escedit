aa32f9241c372f4b038d95b87e536ba53b0edb3b
wwwg <xxperipsis@gmail.com>
2017-10-17 08:21:40 -0500

    Add Strength, Speed, Intellect, and Opinion inputs

--- src/app/InmateCard.js
+++ src/app/InmateCard.js
@@ -4,7 +4,11 @@ class InmateCard {
 		this.i = inmate;
 		this.elm = $(`
 			<div id="inmate-card-${this.num}" class="inmate-card">
-				<span>Name</span> &nbsp; <input id="inmate-name-${this.num}" type="text" value="${inmate[0]}"></span><br>
+				<span style="float:left;">Name</span><input id="inmate-name-${this.num}" class="inmate-name-box" type="text" value="${inmate[0]}"><br>
+				<span style="float:left;">Strength</span><input id="inmate-str-${this.num}" class="inmate-stats-box" type="number" value="${inmate[1]}"></input><br>
+				<span style="float:left;">Speed</span><input id="inmate-speed-${this.num}" class="inmate-stats-box" type="number" value="${inmate[2]}"></input><br>
+				<span style="float:left;">Intellect</span><input id="inmate-int-${this.num}" class="inmate-stats-box" type="number" value="${inmate[3]}"></input><br>
+				<span style="float:left;">Opinion</span><input id="inmate-opi-${this.num}" class="inmate-stats-box" type="number" value="${inmate[4]}"></input>
 			</div>
 		`);
 	}
