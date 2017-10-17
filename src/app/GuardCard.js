964935090a6f003db7fc3e34e8cbc995f7fc897c
wwwg <xxperipsis@gmail.com>
2017-10-17 12:38:23 -0500

    Create GuardCard class

---
+++ src/app/GuardCard.js
@@ -0,0 +1,35 @@
+class GuardCard {
+	constructor(guardNum, guard) {
+		this.g = guard;
+		this.num = guardNum;
+		this.elm = $(`
+			<div id="guard-card-${this.num}" class="inmate-card">
+				<div class="inmate-card-row">
+					<span class="inmate-label">Name</span>
+					<input id="guard-name-${this.num}" class="inmate-name-box" type="text" value="${guard[0]}">
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Strength</span>
+					<input id="guard-str-${this.num}" class="inmate-stats-box" type="number" value="${guard[1]}"></input>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Speed</span>
+					<input id="guard-speed-${this.num}" class="inmate-stats-box" type="number" value="${guard[2]}"></input>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Intellect</span>
+					<input id="guard-int-${this.num}" class="inmate-stats-box" type="number" value="${guard[3]}"></input>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Opinion</span>
+					<input id="guard-opi-${this.num}" class="inmate-stats-box" type="number" value="${guard[4]}"></input>
+				</div>
+			</div>
+		`);
+	}
+	addTo(parent) {
+		$(parent).append(this.elm);
+	}
+}
+
+window.GuardCard = GuardCard;
