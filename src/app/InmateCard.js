b448193d46acd3ae5a959f5f6409b6b46df98e38
wwwg <xxperipsis@gmail.com>
2017-10-17 12:24:26 -0500

    Fix inmate card formatting

--- src/app/InmateCard.js
+++ src/app/InmateCard.js
@@ -4,22 +4,26 @@ class InmateCard {
 		this.i = inmate;
 		this.elm = $(`
 			<div id="inmate-card-${this.num}" class="inmate-card">
-				<span class="inmate-card-row">
-					<span style="float:left;">Name</span>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Name</span>
 					<input id="inmate-name-${this.num}" class="inmate-name-box" type="text" value="${inmate[0]}">
-				</span><br><span class="inmate-card-row">
-					<span style="float:left;">Strength</span>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Strength</span>
 					<input id="inmate-str-${this.num}" class="inmate-stats-box" type="number" value="${inmate[1]}"></input>
-				</span><br><span class="inmate-card-row">
-					<span style="float:left;">Speed</span>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Speed</span>
 					<input id="inmate-speed-${this.num}" class="inmate-stats-box" type="number" value="${inmate[2]}"></input>
-				</span><br><span class="inmate-card-row">
-					<span style="float:left;">Intellect</span>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Intellect</span>
 					<input id="inmate-int-${this.num}" class="inmate-stats-box" type="number" value="${inmate[3]}"></input>
-				</span><br><span class="inmate-card-row">
-					<span style="float:left;">Opinion</span>
+				</div>
+				<div class="inmate-card-row">
+					<span class="inmate-label">Opinion</span>
 					<input id="inmate-opi-${this.num}" class="inmate-stats-box" type="number" value="${inmate[4]}"></input>
-				</span>
+				</div>
 			</div>
 		`);
 	}
