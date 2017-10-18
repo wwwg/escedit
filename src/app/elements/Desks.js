a5df1d5ba2d56e55ea731ffafeea184e6ef81fc2
ultratypebot <xxperipsis@gmail.com>
2017-10-17 20:10:53 -0500

    Add desk selector

--- src/app/elements/Desks.js
+++ src/app/elements/Desks.js
@@ -1,3 +1,17 @@
 window.initDesksTab = () => {
-	// TODO
+	let desks = newSav["Desks"]; // For convenience
+	for (const deskId in desks) {
+		let name = '(none)';
+		if (newSav.Inmates[deskId]) {
+			name = newSav.Inmates[deskId][0] + "'s Desk";
+		} else if (deskId == 0) {
+			// Player's personal desk
+			name = "Your desk";
+		}
+		$("#desk-selector").append(`
+			<option value="${deskId}">
+				${name}
+			</option>
+		`);
+	}
 }
