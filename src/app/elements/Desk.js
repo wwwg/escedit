c88e6cd5624dc0d2d01a5a3d6bbfbbd5dd4daa4a
ultratypebot <xxperipsis@gmail.com>
2017-10-17 21:15:56 -0500

    Change desk layout

--- src/app/elements/Desk.js
+++ src/app/elements/Desk.js
@@ -1,3 +1,12 @@
 window.initDeskTab = () => {
-	let desks = newSav["Desks"]; // For convenience
+	let deskContent = newSav["Desks"][0][3].split('?');
+	for (var i = 0; i < deskContent.length; ++i) {
+		let container = $(`<span class="desk-entry"></span>`),
+			label = $(`<span class="desk-label">${i + 1} &nbsp;</span>`),
+			dropdown = new ItemDropdown(deskContent[i]);
+		container.append(label);
+		dropdown.addTo(container);
+		$("#desk-panel").append(container);
+		$(dropdown.elm).addClass("desk-dropdown");
+	}
 }
