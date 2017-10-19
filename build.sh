3ff6015d30c7a2c44287f9612ea5c0df265e198d
wwwg <xxperipsis@gmail.com>
2017-10-19 11:30:33 -0500

    Build for wider variety of platforms

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ echo "Starting build..."
 export DEBUG=electron-builder,electron-builder:*
 rm -rf ./build/
 mkdir -p build/
-echo "Building for Linux x64 (unpacked)"
-electron-builder --dir --asar --platform=linux --arch=x64
+echo "Building Linux x64..."
+electron-builder --linux --x64 deb tar.gz
+echo "Linux build complete."
+echo "Building Windows ia32..."
+electron-builder --win --ia32
+echo "Build complete."
